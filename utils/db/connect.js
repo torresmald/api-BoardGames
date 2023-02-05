8faf73bedbab571ad4e830eedce60a65a232f6b0
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-02-05 12:43:07 +0100

    fixed password intrusion

--- utils/db/connect.js
+++ utils/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const DB_URL = 'mongodb+srv://root:b7J2VPGRF8cPYcJT@boardgames.4thz9eh.mongodb.net/?retryWrites=true&w=majority'
+const DB_URL = process.env.DB_URL;
 
 const connect = () => {
     mongoose.connect(DB_URL, {
