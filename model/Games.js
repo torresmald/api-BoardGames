420fd24b272ad52b0a72f92d31da60ec783a3c27
Jonathan Torres <jonathan.torresmald@gmail.com>
2023-02-09 17:31:33 +0100

    fixed playingTime

--- model/Games.js
+++ model/Games.js
@@ -10,7 +10,7 @@ const boardGamesSchema = new mongoose.Schema({
     picture: String,
     players: { type: [String], enum: ['1 Jugador', '2 Jugadores', '4 Jugadores', '1/4 Jugadores', '1/5 Jugadores','2/4 Jugadores', '2/5 Jugadores','2/8 Jugadores', '2/10 Jugadores', '3/4 Jugadores','5 Jugadores', '5+ Jugadores'] },
     weight: { type: Number, min: 1, max: 5 },
-    playingTime: { type: [String], enum: ['-15 Minutos', '15 Minutos', '30 Minutos', '30/60 Minutos', '1 Hora', '1/2 Horas', '2/3 Horas', '+3 Horas']},
+    playingTime: { type: String, enum: ['-15 Minutos', '15 Minutos', '30 Minutos', '30/60 Minutos', '1 Hora', '1/2 Horas', '2/3 Horas', '+3 Horas']},
     isForChilds: Boolean,
     favoriteCount: Number
 },
